2a217963e916e0d3ad179d74ddf2b942d5a48e77
shortpoet <soriano.carlitos@gmail.com>
2020-08-02 11:16:52 -0500

    add category

--- test/viewModels/projects.spec.ts
+++ test/viewModels/projects.spec.ts
@@ -1,6 +1,7 @@
 import { RestProject, projectViewModel } from "../../src/viewModels/projects";
-import { Connection, createConnection, getRepository, DeepPartial } from "typeorm";
+import { Connection, createConnection, getRepository } from "typeorm";
 import { Project } from "../../src/entity/Project";
+import { createProject } from "../projects";
 
 let connection: Connection;
 
@@ -13,13 +14,6 @@ beforeAll(async () => {
 afterAll(async () => {
   await connection.close();
 });
-
-const createProject = (attrs: DeepPartial<Project>): Promise<Project> => {
-  const repo = getRepository(Project);
-  return repo.save(<Project>{
-    name: attrs.name || 'My new project'
-  });
-};
 
 describe('projects.ts', () => {
   test('project view model', async () => {
