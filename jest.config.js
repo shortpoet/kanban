8889b38655d894ca91d112da9ffa58ea9f797771
shortpoet <soriano.carlitos@gmail.com>
2020-08-02 16:40:39 -0500

    articles

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,7 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   // https://github.com/facebook/jest/issues/1395#issuecomment-419490847
+  // https://docs.docker.com/docker-for-mac/osxfs-caching/
+  // https://medium.com/@kevinsimper/how-to-disable-jsdom-in-jest-make-jest-run-twice-as-fast-a01193f23405
   roots: ["./test"]
 };
