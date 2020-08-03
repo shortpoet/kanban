340db4dfbcbc4bee5d86e633ba4aa333107591f7
shortpoet <soriano.carlitos@gmail.com>
2020-08-02 19:12:37 -0500

    add some linux stuff to docker

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,7 +4,7 @@ BrownOrange="\033[0;33m"
 
 set -e
 
-echo "${Green}The Dockerfile ${BrownOrange}ENTRYPOINT ${Green}has been executed!"
+echo -e "${Green}The Dockerfile ${BrownOrange}ENTRYPOINT ${Green}has been executed!"
 
 # export MY_DIR="${PWD:2}"
 
