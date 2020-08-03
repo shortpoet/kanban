82b74cdb437ee474985377b318217f17fcbbf9e7
shortpoet <soriano.carlitos@gmail.com>
2020-08-03 12:33:06 -0500

    add volume persist; make all scripts executable

--- docker/test.sh
+++ docker/test.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # https://stackoverflow.com/questions/59895/how-to-get-the-source-directory-of-a-bash-script-from-within-the-script-itself
 # https://stackoverflow.com/questions/45546548/import-variables-into-bash-script-from-a-config-file
 # https://unix.stackexchange.com/questions/216910/bash-command-to-source-a-file-in-a-different-directory
