b28e45dbbfc2ed6c27cbe1e3d63b0708abaaa29e
shortpoet <soriano.carlitos@gmail.com>
2020-08-07 11:21:43 -0500

    db linux compatible with :Z flag on vol mount

--- docker/db-shell.sh
+++ docker/db-shell.sh
@@ -2,6 +2,11 @@
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 source "$DIR/colors.cfg"
-echo -e "${Cyan}The ${Yellow}postgres ${Cyan}shell script has been executed"
 
-winpty docker exec -it kanban_db_1 bash "$@"
+if [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
+    echo -e "${Cyan}The ${Yellow}postgres windows ${Cyan}shell script has been executed"
+    winpty docker exec -it kanban_db_1 bash "$@"
+else 
+    echo -e "${Cyan}The ${Yellow}postgres ${Cyan}shell script has been executed"
+    docker exec -it kanban_db_1 bash "$@"
+fi;
